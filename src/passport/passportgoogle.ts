1e2451ebee4ca39474d58a3667ebd745948383de
Lucas Vidor Migotto <lmigotto@amix.com.br>
2018-04-05 16:24:39 -0300

    Passport google criado

---
+++ src/passport/passportgoogle.ts
@@ -0,0 +1,27 @@
+import * as Google from 'passport-google-oauth'
+import { default as User, UserModel } from '../model/users'
+import { default as config } from '../config/auth'
+
+export default (passport: any) => {
+  passport.use(new Google.OAuth2Strategy({
+    clientID: config.googleAuth.clientID,
+    clientSecret: config.googleAuth.clientSecret,
+    callbackURL: config.googleAuth.callbackURL
+  }, (token, refreshToken, profile, done) => {
+    process.nextTick(() => {
+      User.findOne({ id: profile.id },
+        (err: Error, user: UserModel) => {
+          if (err) return done(null, { message: err.message });
+          if (user) {
+            return done(null, user);
+          } else {
+            let newUser = new User(user);
+            newUser.save((err: Error) => {
+              if (err) return done(null, { message: err.message });
+              return done(null, newUser)
+            })
+          }
+        })
+    })
+  }))
+}
