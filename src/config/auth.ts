b1518bf058430991ab20ca94e84db704ea613625
Lucas Vidor Migotto <lmigotto@amix.com.br>
2018-04-05 16:23:24 -0300

    Keys do Facebook e Google

---
+++ src/config/auth.ts
@@ -0,0 +1,12 @@
+export default {
+  'facebookAuth': {
+    'clientID': '1799181683717137',
+    'clientSecret': '11b6170d065446748a448a37f1841050',
+    'callbackURL': 'https://758834a2.ngrok.io/api/facebook/callback'
+  },
+  'googleAuth': {
+    'clientID': '153187670921-bavg21i0hvjs88m40g0rmada9qgigrkt.apps.googleusercontent.com',
+    'clientSecret': 'mvTLjXdumhzc0RujhFl1T4kh',
+    'callbackURL': 'https://758834a2.ngrok.io/api/google/callback'
+  }
+}
