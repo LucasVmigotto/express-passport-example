0fbdd8adb31cd1e1513ee66667f72293be61f644
Lucas Vidor Migotto <lmigotto@amix.com.br>
2018-04-06 12:51:58 -0300

    Usuario agora pode ter tres contas

--- src/model/users.ts
+++ src/model/users.ts
@@ -4,21 +4,45 @@
 import * as mongoose from 'mongoose';
 
 export type UserModel = mongoose.Document & {
-  id: number,
-  displayname: string,
-  username: string,
-  password: string,
-  token: string
+  local: {
+    name: String,
+    email: String,
+    password: String
+  },
+  facebook: {
+    id: String,
+    email: String,
+    token: String,
+    name: String
+  },
+  google: {
+    id: String,
+    email: String,
+    token: String,
+    name: String
+  }
 }
 /**
  * Create a UserSchema using mongoose schema
  */
 let UserSchema: mongoose.Schema = new mongoose.Schema({
-  id: Number,
-  displayname: String,
-  username: String,
-  password: String,
-  token: String
+  local: {
+    name: String,
+    email: String,
+    password: String,
+  },
+  facebook: {
+    id: String,
+    email: String,
+    token: String,
+    name: String
+  },
+  google: {
+    id: String,
+    email: String,
+    token: String,
+    name: String
+  }
 });
 /**
  * Export mongoose model UserSchema
