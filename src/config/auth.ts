e45d345f8f48d994a16e2d854a99f677e35a9e11
Lucas Vidor Migotto <lmigotto@amix.com.br>
2018-04-06 12:49:29 -0300

    Callback atualizado

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -2,11 +2,11 @@ export default {
   'facebookAuth': {
     'clientID': '166343247416451',
     'clientSecret': '6296e642f15f8dd95760e482603c2e32',
-    'callbackURL': 'https://d884d67f.ngrok.io/api/facebook/callback'
+    'callbackURL': 'https://25c95cda.ngrok.io/api/facebook/callback'
   },
   'googleAuth': {
     'clientID': '153187670921-bavg21i0hvjs88m40g0rmada9qgigrkt.apps.googleusercontent.com',
     'clientSecret': 'mvTLjXdumhzc0RujhFl1T4kh',
-    'callbackURL': 'https://d884d67f.ngrok.io/api/google/callback'
+    'callbackURL': 'https://25c95cda.ngrok.io/api/google/callback'
   }
 }
