74b8aa4afd534dddcfcc10f64d5f46ee83325fd7
Lucas Vidor Migotto <lmigotto@amix.com.br>
2018-04-06 12:53:20 -0300

    Melhoria de codigo

--- src/passport/passportgoogle.ts
+++ src/passport/passportgoogle.ts
@@ -3,25 +3,23 @@ import { default as User, UserModel } from '../model/users'
 import { default as config } from '../config/auth'
 
 export default (passport: any) => {
-  passport.use(new Google.OAuth2Strategy({
-    clientID: config.googleAuth.clientID,
-    clientSecret: config.googleAuth.clientSecret,
-    callbackURL: config.googleAuth.callbackURL
-  }, (token, refreshToken, profile, done) => {
-    process.nextTick(() => {
-      User.findOne({ username: profile.username },
-        (err: Error, user: UserModel) => {
-          if (err) return done(null, { message: err.message });
-          if (user) {
-            return done(null, user);
-          } else {
-            let newUser = new User(user);
-            newUser.save((err: Error) => {
-              if (err) return done(null, { message: err.message });
-              return done(null, newUser)
-            })
-          }
-        })
-    })
-  }))
+  passport.use(new Google.OAuth2Strategy(config.googleAuth,
+    (token, refreshToken, profile, done) => {
+      process.nextTick(() => {
+        User.findOne({ 'google.id': profile.id },
+          (err: Error, user: UserModel) => {
+            if (err) return done(null, { message: err.message });
+            if (user) {
+              return done(null, user);
+            } else {
+              let newUser = new User(user);
+              newUser.save((err: Error) => {
+                if (err) return done(null, { message: err.message });
+                return done(null, newUser)
+              })
+            }
+          })
+      })
+    }
+  ))
 }
